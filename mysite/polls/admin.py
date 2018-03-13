e16fe3d4f47b2cd88276ecf70125eb29d4d592fa
Daniel Duffield <danieljduffield@gmail.com>
2018-03-13 16:36:38 -0700

    Added choices displayed inline tabular.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from polls.models import Question
+from polls.models import Question, Choice
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, { 'fields': ['question_text']}),
         ('Date information', { 'fields': ['pub_date']}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
