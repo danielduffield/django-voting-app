5476a69407f446bada270e29f6523719cd0875e2
Daniel Duffield <danieljduffield@gmail.com>
2018-03-13 16:27:01 -0700

    Updated the polls admin settings, adding QuestionAdmin class to modify display in admin backend.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from polls.models import Question
 
-# Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, { 'fields': ['question_text']}),
+        ('Date information', { 'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
